0782fb639b83813e8718102ae444fe83d784ef64
Evan Chen <echen10@stuy.edu>
2017-02-06 16:44:52 -0500

    Added Recursion lab directory

---
+++ git01Recursion/Recursion.java
@@ -0,0 +1,12 @@
+public class Recursion {
+    public static String name() {
+	return "Chen,Evan";
+    }
+
+    private boolean isCloseEnough() {
+    }
+    
+    public static double sqrt(double n) {
+
+    }
+}
