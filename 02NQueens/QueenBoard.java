fc9543e1530f23a89c52290602e8e86dbf79b174
Evan Chen <echen10@stuy.edu>
2017-02-13 23:26:11 -0500

    Uhh..I should have commit this earlier

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,21 +1,52 @@
 public class QueenBoard{
     private int[][] board;
-    private int solutionCount;
+    private int solutionCount = -1;
     
     public QueenBoard(int size){
 	board = new int[size][size];
     }
 
-    public boolean solve() {
+    private void addQueen(int r, int c) {
+	board[r][c]++;
+	
+	//updateBoardState
+	//right side
+	for ( int col = c; col < board[col].length; col++ ) {
+	    board[r][col]++;
+	}
+	//upper diagonal
+	for ( int col = c+1, int row = r+1; col < board[col].length || row < 0; col++, row-- ) {
+	    board[row][col]
+	//lower diagonal
+	
+    }
+
+    private void removeQueen(int r, int c) {
+    }
+    
+    public void solve() {
+	
 	return solveH(0);
     }
 
     private boolean solveH(int col) {
-	return false;
+	if ( col > board.size() ) {
+	    return solutionCount == board.length;
+	}
+	for ( int row = 0; row < board[col].size; r++ ) {
+	    if ( board[col][row] == 0 ) {
+		addQueen(row,col);
+		return solveH(col+1);
+	    }	
+	}
+	return solveH(col+1);
     }
 
-    public int getSolutionCount() {
-    	return -1;
+    public void countSolutions() {
+    }
+    
+    public int getCount() {
+    	return solutionCount;
     }
 
     public String toString() {
