67cd51bcf7488d0825e2d66c65e9bb23848ad9ab
echen10 <echen10@stuy.edu>
2017-02-15 20:10:49 -0500

    Create 03Knight

---
+++ 03Knight/KnightBoard.java
@@ -0,0 +1,18 @@
+public class KnightBoard() {
+    private int[][] board;
+
+    public KnightBoard( int rows, int cols ) {
+	board = new int[rows][cols];
+    }
+
+    public void solve() {
+    }
+
+    private boolean solveH() {
+    }
+
+
+
+    public String toString() {
+	return "";
+    }
