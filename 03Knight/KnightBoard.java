0d97178dac3dc1f39f13cbfdeb625227dd4dda84
echen10 <echen10@stuy.edu>
2017-02-16 20:33:47 -0500

    .

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -6,13 +6,31 @@ public class KnightBoard() {
     }
 
     public void solve() {
+	solveH(0,0,1);
     }
 
-    private boolean solveH() {
+    private boolean solveH(int row, int col, int level) {
+	if ( level >= row*col ) {
+	    return true;
+	}
+	board[row+r][col+c] = level;
+	solveH(r,c,level);
     }
 
-
+    private boolean isGoodSpot(int row, int col) {
+	if ( board[row-1][col-2] != 0 ||
+	     board[row-2][col-1] != 0 ||
+	     board[row-2][col+1] != 0 ||
+	     board[row-1][col+2] != 0 ||
+	     board[row+1][col+2] != 0 ||
+	     board[row+2][col+1] != 0 ||
+	     board[row+2][col-1] != 0 ||
+	     board[row+1][col-2] != 0 ) {
+	    return true;
+	}
+    }
 
     public String toString() {
 	return "";
     }
+}
