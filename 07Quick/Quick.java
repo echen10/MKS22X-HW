b22d5a0803fde06e62bccf3584546f43abfc64c6
Evan Chen <echen10@stuy.edu>
2017-03-16 00:36:52 -0400

    While loops are cleaner for partition

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,30 +1,36 @@
 public class Quick {
     public static int part( int[] data, int start, int end ) {
-	int piv = data[ (int)( Math.random() * data.length ) ];
-	int pivIndex = 0;
-	int last = data.length;
-	int[] temp = new int[data.length]; //change to in place
-	for ( int i = start; i < end; i++ ) {
-	    if ( data[i] < piv ) {
-		temp[i] = data[i];
+	int piv = data[ (int)( Math.random() * ( end-start ) ) ];
+	while ( start < end ) {
+	    while ( data[start] < piv ) {
+		start++;
 	    }
-	    if ( data[i] > piv ) {
-		temp[last] = data[i];
-		last--;
+	    while ( data[end] > piv ) {
+		end--;
 	    }
-	    if ( data[i] == piv ) {
-		pivIndex = data[i];
+	    if ( start < end ) {
+		swap( data, start, end );
 	    }
 	}
-	return pivIndex;
-	
-	
+	return start;
+    }
+
+    private static void swap( int[] data, int a, int b ) {
+	int temp = data[a];
+	data[a] = data[b];
+	data[b] = temp;
     }
     
     public static void main( String[] args ) {
 	Quick q = new Quick();
-	int[] ary = new int[] { 1,2,3,4,5 };
+	int[] ary = new int[] { 6,3,2,1,9 };
+	for ( int i = 0; i < ary.length; i++ ) {
+	    System.out.println(ary[i]);
+	}
 	q.part( ary, 0, 4 );
+	for ( int i = 0; i < ary.length; i++ ) {
+	    System.out.println(ary[i]);
+	}
     }
 
 }
