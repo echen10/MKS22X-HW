6b6b3c5a09de4d46f1a71adbf584c909deb53535
echen10 <echen10@stuy.edu>
2017-03-23 03:35:20 -0400

    Some changes

--- 08MergeSort/Merge.java
+++ 08MergeSort/Merge.java
@@ -5,7 +5,8 @@ public class Merge {
     public static void merge( int[] a, int[] b, int[] dest ) {
 	int i = 0, j = 0;
 	int index = 0;
-	while ( dest.length > a.length + b.length ) {
+	int[] d = int[a.length+.length]
+	while ( dest.length-2 < i+j ) {
 	    if ( a[i] > b[j] ) {
 		dest[index] = b[j];
 		index++;
