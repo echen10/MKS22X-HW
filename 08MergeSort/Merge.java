2ee20e7210f2474b53c13956e207b08622e6e3d1
Evan Chen <echen10@stuy.edu>
2017-03-23 17:14:00 -0400

    Problem with updating destination array

--- 08MergeSort/Merge.java
+++ 08MergeSort/Merge.java
@@ -2,10 +2,10 @@ public class Merge {
     public static void mergesort( int[] ary ) {
     }
 
-    public static void merge( int[] a, int[] b, int[] dest ) {
+    public void merge( int[] a, int[] b, int[] dest ) {
 	int i = 0, j = 0;
 	int index = 0;
-	int[] d = int[a.length+.length]
+	int[] d = new int[ a.length + b.length ];
 	while ( dest.length-2 < i+j ) {
 	    if ( a[i] > b[j] ) {
 		dest[index] = b[j];
