8c03f6af6f17e20f3cc4e34f9d2750210d1ad45a
Evan Chen <echen10@stuy.edu>
2017-03-28 23:54:28 -0400

    Finished sort

--- 08MergeSort/Merge.java
+++ 08MergeSort/Merge.java
@@ -1,9 +1,23 @@
 public class Merge {
+    
     public static void mergesort( int[] ary ) {
-	
+	if ( ary.length <= 1 ) {
+	    return;
+	}
+	else {
+	    int[] left = new int[ ary.length / 2 ];
+	    fill( 0, ary.length/2, ary, left );
+	    int[] right = new int[ ary.length / 2 ];
+	    fill( ary.length/2, ary.length, ary, right );
+	    
+	    mergesort(left);
+	    mergesort(right);
+	    merge(left, right, ary);
+	}
+		
     }
 
-    public void merge( int[] a, int[] b, int[] dest ) {
+    public static void merge( int[] a, int[] b, int[] dest ) {
 
 	int i = 0, j = 0, count = 0;
 	while ( i < a.length && j < b.length ) {
@@ -18,8 +32,6 @@ public class Merge {
 	    count++;
 	}
 
-	//To add the rest of the values:
-
 	while ( i < a.length ) {
 	    dest[count] = a[i];
 	    i++;
@@ -31,36 +43,12 @@ public class Merge {
 	    count++;
 	}
 
+    }
 
-
-	/*   This didn't work...
-	int total = a.length + b.length;
-	for ( int count = 0, i = 0, j = 0; i < a.length && j < b.length ; count++ ) {
-	    if ( i == a.length-1 ) {
-		for ( int m = j; j < b.length; j++ ) {
-		    dest[count] = b[m];
-		}
-	    }
-	    else if ( j == b.length-1 ) {
-		for ( int n = i; i < a.length; n++ ) {
-		    dest[count] = a[n];
-		}
-	    }
-	    else if ( a[i] < b[j] ) {
-		dest[count] = a[i];
-		if ( i < a.length ) {
-		    i++;
-		}
-	    }
-	    else {
-		dest[count] = b[j];
-		if ( j < b.length ) {
-		    j++;
-		}
-	    }
+    private static void fill( int start, int end, int[] ary, int[] dest ) {
+	for ( int i = start, count = 0; i < end-start; i++,count++ ) {
+	    dest[count] = ary[i];
 	}
-	*/
-
     }
 	
 }
