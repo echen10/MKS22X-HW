7d4b7a8f31884d0dbbd5d3db947ba4206195fdde
echen10 <echen10@stuy.edu>
2017-04-21 12:29:29 -0400

    Worked on Postfix

--- 11PostfixEval/Postfix.java
+++ 11PostfixEval/Postfix.java
@@ -1,4 +1,38 @@
+import java.util.*;
 public class Postfix {
     public static double eval( String s ) {
 	String[] tokens = s.split(" ");
-	
+	Stack<String> nums = new Stack<String>();
+
+	nums.push(tokens[0]);
+	nums.push(tokens[1]);
+	for ( int i = 2; i < tokens.length; i++ ) {
+	    if ( isOp(tokens[i]) ) {
+		String a = nums.pop();
+		String b = nums.pop();
+		calc( tokens[i], a, b );
+	    }
+	    else {
+		nums.push(tokens[i]);
+	    }
+	}
+	return 0.0;
+    }
+
+    public static boolean isOp( String s ) {
+	if ( s.compareTo("+") == 0 ) {
+	    return true;
+	}
+	else { 
+	    return false;
+	}
+    }
+
+    public static double calc( String op, String a, String b ) {
+	return 0.0;
+    }
+    
+    public static void main( String[] args ) {
+	eval("S t o p");
+    }
+}
