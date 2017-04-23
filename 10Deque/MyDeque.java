86bc89156a4a2ce854a0bb12104ea494604d61f4
Evan Chen <echen10@stuy.edu>
2017-04-23 15:34:32 -0400

    Basic outline

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,2 +1,38 @@
-public interface MyDeque extends Queue {
+public class MyDeque {
     
+    public void addFirst( String s ) {
+	if ( if s == null ) {
+	    throw new NullPointerException();
+	}
+    }
+
+    public void addLast( String s ) {
+	if ( if s == null ) {
+	    throw new NullPointerException();
+	}
+    }
+
+    public String removeFirst() {
+	if () {
+	    throw new NoSuchElementException();
+	}
+    }
+
+    public String removeLast() {
+	if () {
+	    throw new NoSuchElementException();
+	}
+    }
+
+    public String getFirst() {
+	if () {
+	    throw new NoSuchElementException();
+	}
+    }
+
+    public String getLast() {
+	if () {
+	    throw new NoSuchElementException();
+	}
+    }
+}
