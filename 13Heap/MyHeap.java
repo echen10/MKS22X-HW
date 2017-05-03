9fdc0ed8cb9217da692a6b2c5cd0bae8f068d590
echen10 <echen10@stuy.edu>
2017-05-03 12:33:28 -0400

    Heap started

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,25 @@
+public class MyHeap {
+    
+    public MyHeap(boolean minmax) {
+	if ( minmax ) {
+
+	}
+	else {
+
+	}
+    }
+
+    public void add(){
+
+    }
+    public void remove(){
+
+    }
+    
+
+
+    public String toString() {
+	return "";
+    }
+
+}
