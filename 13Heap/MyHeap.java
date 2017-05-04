0112dfaaa49bf00a42752e2718a2271d5c4051c1
echen10 <echen10@stuy.edu>
2017-05-04 12:25:07 -0400

    toString

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,25 +1,57 @@
+import java.util.*;
 public class MyHeap {
-    
-    public MyHeap(boolean minmax) {
-	if ( minmax ) {
+    private boolean max;
+    private ArrayList<String> tree;
 
+    public MyHeap() {
+	max = true;
+	tree = new ArrayList<String>();
+	tree.add(null);
+    }
+    /*
+    public MyHeap( boolean type ) {
+	if ( type ) {
+	    max = true;
 	}
 	else {
-
+	    max = false;
 	}
     }
+    */
 
-    public void add(){
-
+    public void add( String s ) {
+	tree.add(s);
     }
-    public void remove(){
+    /*
+    public void remove() {
 
     }
     
+    public String peek() {
 
+    }
+    */
 
     public String toString() {
-	return "";
+	String ans = "";
+        int count = 0;
+	for ( int i = 1; i < tree.size(); i++ ) {
+	    if ( i == Math.pow(2,count) ) {
+		ans += tree.get(i) + " ";
+		ans += "\n";
+		count++;
+	    }
+	    else {
+		ans += tree.get(i) + " ";
+	    }
+	}
+	return ans;
     }
 
+    public static void main( String[] args ) {
+	MyHeap m = new MyHeap();
+	System.out.println(m);
+	m.add("A");
+	m.add("B");
+    }
 }
